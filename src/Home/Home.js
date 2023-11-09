b688d585aa73916131b7437474f09cb8b6cc4859
gayatrisathawane <gayatripsathawane@gmail.com>
2023-11-09 08:52:31 +0530

    .env config

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './Home.css'
 import { useSearchParams } from 'react-router-dom';
 import { useState } from 'react';
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config();
+
 
 const Home = () => {
     let greetings  = ["“Wishing you a bright and happy Diwali!” “Prayers for love and light to you and your family!From our family to yours, wishes for love, peace and prosperity!” “May hope and happiness shine on you.”", 
@@ -13,15 +14,9 @@ const Home = () => {
     "Let us keep Diwali holding it close to our hearts for its meaning never ends and its spirit is the warmth and joy of remembering friend",]
   
 
-    const themes = {
-        "theme-1":"theme1",
-        "theme-2":"theme2",
-        "theme-3":"theme3",
-        "theme-4":"theme4",
-    }
+  
 
     const [searchParams] = useSearchParams();
-
     const [to,setTo] = useState(searchParams.get('to'))
     const [from,setFrom ]= useState(searchParams.get('from'))
     const [greetingNumber,setGreetingNumber]= useState(searchParams.get('g') >= greetings.length ? 
@@ -32,7 +27,7 @@ const Home = () => {
     
    return (
     <div>
-        <div className={`greeting-container ${theme}`}>
+        <div className={`greeting-container theme-${theme}`}>
             <h4>Dear {to}</h4>
             <h3>{greetings[greetingNumber]}  </h3>
             <h4 className='from-text'>-{from}</h4>
@@ -40,7 +35,9 @@ const Home = () => {
 
         <div>
 
-        <p className='url'>{`${process.env.PORT}/?to=${to}&from=${from}&g=${greetingNumber}&t=${theme}`}</p>
+          <p>Do you want to create your own Diwali Greeting ? customize it  here 👇</p>
+
+        <p className='url'>{`${process.env.REACT_APP_BASE_URL}?to=${to}&from=${from}&g=${greetingNumber}&t=${theme}`}</p>
 
         </div>
           
@@ -67,19 +64,19 @@ const Home = () => {
             <select  onClick={(e)=>{
                 setGreetingNumber(e.target.value)
             }}>
-                <option value="1">greet 1</option>
-                <option value="2">greet 2</option>
-                <option value="3">greet 3</option>
-                <option value="4">greet 4</option>
+                <option value="0">greet 1</option>
+                <option value="1">greet 2</option>
+                <option value="2">greet 3</option>
+                <option value="3">greet 4</option>
             </select>
 
             <select  onClick={(e)=>{
                 setTheme(e.target.value)
             }}>
-                <option value="theme-1">theme 1</option>
-                <option value="theme-2">theme 2</option>
-                <option value="theme-3">theme 3</option>
-                <option value="theme-4">theme 4</option>
+                <option value="1">theme 1</option>
+                <option value="2">theme 2</option>
+                <option value="3">theme 3</option>
+                <option value="4">theme 4</option>
             </select>
           </div>
     </div>
